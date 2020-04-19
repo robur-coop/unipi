2ce1cfbe3a7ce336077c72987426811f7ad83912
Hannes Mehnert <hannes@mehnert.org>
2020-04-19 17:38:51 +0200

    cleanups

--- unikernel.ml
+++ unikernel.ml
@@ -7,124 +7,135 @@ module Main (S: Mirage_stack.V4) (RES: Resolver_lwt.S) (CON: Conduit_mirage.S) (
   module Store = Irmin_mirage_git.Mem.KV(Irmin.Contents.String)
   module Sync = Irmin.Sync(Store)
 
-  let ssh_config () =
-    match Astring.String.cut ~sep:"://" (Key_gen.remote ()) with
-    | Some (pre, _) when Astring.String.is_infix ~affix:"ssh" pre ->
-      begin
-        match Key_gen.ssh_seed (), Key_gen.ssh_authenticator () with
-        | None, _ -> invalid_arg "no ssh key seed provided, but ssh git remote"
-        | Some seed, None ->
-          Logs.warn (fun m -> m "ssh server will not be authenticated");
-          Cohttp.Header.init_with "config" (seed ^ ":")
-        | Some seed, Some authenticator ->
-          Cohttp.Header.init_with "config" (seed ^ ":" ^ authenticator)
-      end
-    | _ -> Cohttp.Header.init ()
-
-  let decompose_git_url () =
-    match String.split_on_char '#' (Key_gen.remote ()) with
-    | [ url ] -> Ok (url, None)
-    | [ url ; branch ] -> Ok (url, Some branch)
-    | _ -> Error (`Msg "expected at most a single # in remote")
-
-  let connect_store resolver conduit =
-    match decompose_git_url () with
-    | Ok (uri, branch) ->
-      let config = Irmin_mem.config () in
-      Store.Repo.v config >>= fun r ->
-      (match branch with
-       | None -> Store.master r
-       | Some branch -> Store.of_branch r branch) >|= fun repo ->
-      let headers = ssh_config () in
-      Ok (repo, Store.remote ~headers ~conduit ~resolver uri)
-    | Error _ as e -> Lwt.return e
-
-  let ptime_to_http_date ptime =
-    let (y, m, d), ((hh, mm, ss), _) = Ptime.to_date_time ptime
-    and weekday = match Ptime.weekday ptime with
-      | `Mon -> "Mon" | `Tue -> "Tue" | `Wed -> "Wed" | `Thu -> "Thu"
-      | `Fri -> "Fri" | `Sat -> "Sat" | `Sun -> "Sun"
-    and month =
-      [|"Jan";"Feb";"Mar";"Apr";"May";"Jun";"Jul";"Aug";"Sep";"Oct";"Nov";"Dec"|]
+  module Last_modified = struct
+    let ptime_to_http_date ptime =
+      let (y, m, d), ((hh, mm, ss), _) = Ptime.to_date_time ptime
+      and weekday = match Ptime.weekday ptime with
+        | `Mon -> "Mon" | `Tue -> "Tue" | `Wed -> "Wed" | `Thu -> "Thu"
+        | `Fri -> "Fri" | `Sat -> "Sat" | `Sun -> "Sun"
+      and month =
+        [| "Jan" ; "Feb" ; "Mar" ; "Apr" ; "May" ; "Jun" ;
+           "Jul" ; "Aug" ; "Sep" ; "Oct" ; "Nov" ; "Dec" |]
     in
     let m' = Array.get month (pred m) in
     Printf.sprintf "%s, %02d %s %04d %02d:%02d:%02d GMT" weekday d m' y hh mm ss
 
-  (* cache the last commit (last modified and last hash) *)
-  let last = ref ("", "")
+    (* cache the last commit (last modified and last hash) *)
+    let last = ref ("", "")
 
-  (* cache control: all resources use last-modified + etag of last commit *)
-  let retrieve_last_commit store =
-    Store.Head.get store >|= fun head ->
-    let last_commit_date =
-      let info = Store.Commit.info head in
-      let ptime =
-        match Ptime.of_float_s (Int64.to_float (Irmin.Info.date info)) with
-        | None -> Ptime.v (C.now_d_ps ())
-        | Some d -> d
+    (* cache control: all resources use last-modified + etag of last commit *)
+    let retrieve_last_commit store =
+      Store.Head.get store >|= fun head ->
+      let last_commit_date =
+        let info = Store.Commit.info head in
+        let ptime =
+          match Ptime.of_float_s (Int64.to_float (Irmin.Info.date info)) with
+          | None -> Ptime.v (C.now_d_ps ())
+          | Some d -> d
+        in
+        ptime_to_http_date ptime
+      and last_commit_hash =
+        Fmt.to_to_string (Irmin.Type.pp Store.Hash.t) (Store.Commit.hash head)
       in
-      ptime_to_http_date ptime
-    and last_commit_hash =
-      Fmt.to_to_string (Irmin.Type.pp Store.Hash.t) (Store.Commit.hash head)
-    in
-    last := (last_commit_date, last_commit_hash)
+      last := (last_commit_date, last_commit_hash)
 
-  let pull_store store upstream =
-    Logs.info (fun m -> m "pulling from remote!");
-    Sync.pull store upstream `Set >>= fun r ->
-    retrieve_last_commit store >|= fun () ->
-    match r with
-    | Ok (`Head _ as s) -> Ok (Fmt.strf "pulled %a" Sync.pp_status s)
-    | Ok `Empty -> Error (`Msg "pulled empty repository")
-    | Error (`Msg e) -> Error (`Msg ("pull error " ^ e))
-    | Error (`Conflict msg) -> Error (`Msg ("pull conflict " ^ msg))
+    let not_modified request =
+      let hdr = request.Cohttp.Request.headers in
+      match Cohttp.Header.get hdr "if-modified-since" with
+      | Some ts -> String.equal ts (fst !last)
+      | None -> match Cohttp.Header.get hdr "if-none-match" with
+        | Some etags -> List.mem (snd !last) (Astring.String.cuts ~sep:"," etags)
+        | None -> false
 
-  let not_modified request =
-    let hdr = request.Cohttp.Request.headers in
-    match Cohttp.Header.get hdr "if-modified-since" with
-    | Some ts -> String.equal ts (fst !last)
-    | None -> match Cohttp.Header.get hdr "if-none-match" with
-      | Some etags -> List.mem (snd !last) (Astring.String.cuts ~sep:"," etags)
-      | None -> false
+    let last_modified, etag = !last
+  end
 
-  let dispatch store hook request _body =
-    let p = Uri.path (Cohttp.Request.uri request) in
-    let path = if String.equal p "/" then "index.html" else p in
-    Logs.info (fun f -> f "requested %s" path);
-    match Astring.String.cuts ~sep:"/" ~empty:false path with
-    | [ h ] when String.equal (Key_gen.hook ()) h ->
-      begin
-        hook () >>= function
-        | Ok data -> Http.respond ~status:`OK ~body:(`String data) ()
-        | Error (`Msg msg) ->
-          Http.respond ~status:`Internal_server_error ~body:(`String msg) ()
-      end
-    | path_list ->
-      if not_modified request then
-        Http.respond ~status:`Not_modified ~body:`Empty ()
-      else
-        Store.find store path_list >>= function
-        | Some data ->
-          let mime_type = Magic_mime.lookup path in
-          let headers = [
-            "content-type", mime_type ;
-            "etag", snd !last ;
-            "last-modified", fst !last
-          ] in
-          let headers = Cohttp.Header.of_list headers in
-          Http.respond ~headers ~status:`OK ~body:(`String data) ()
-        | None ->
-          let data = "Resource not found " ^ path in
-          Http.respond ~status:`Not_found ~body:(`String data) ()
+  module Remote = struct
+    let ssh_config () =
+      match Astring.String.cut ~sep:"://" (Key_gen.remote ()) with
+      | Some (pre, _) when Astring.String.is_infix ~affix:"ssh" pre ->
+        begin
+          match Key_gen.ssh_seed (), Key_gen.ssh_authenticator () with
+          | None, _ -> invalid_arg "no ssh key seed provided, but ssh git remote"
+          | Some seed, None ->
+            Logs.warn (fun m -> m "ssh server will not be authenticated");
+            Cohttp.Header.init_with "config" (seed ^ ":")
+          | Some seed, Some authenticator ->
+            Cohttp.Header.init_with "config" (seed ^ ":" ^ authenticator)
+        end
+      | _ -> Cohttp.Header.init ()
 
-  let redirect port request _body =
-    let uri = Cohttp.Request.uri request in
-    let new_uri = Uri.with_scheme uri (Some "https") in
-    let new_uri = Uri.with_port new_uri (Some port) in
-    Logs.info (fun f -> f "[%s] -> [%s]"
-                  (Uri.to_string uri) (Uri.to_string new_uri));
-    let headers = Cohttp.Header.init_with "location" (Uri.to_string new_uri) in
-    Http.respond ~headers ~status:`Moved_permanently ~body:`Empty ()
+    let decompose_git_url () =
+      match String.split_on_char '#' (Key_gen.remote ()) with
+      | [ url ] -> Ok (url, None)
+      | [ url ; branch ] -> Ok (url, Some branch)
+      | _ -> Error (`Msg "expected at most a single # in remote")
+
+    let connect resolver conduit =
+      match decompose_git_url () with
+      | Ok (uri, branch) ->
+        let config = Irmin_mem.config () in
+        Store.Repo.v config >>= fun r ->
+        (match branch with
+         | None -> Store.master r
+         | Some branch -> Store.of_branch r branch) >|= fun repo ->
+        let headers = ssh_config () in
+        Ok (repo, Store.remote ~headers ~conduit ~resolver uri)
+      | Error _ as e -> Lwt.return e
+
+    let pull store upstream =
+      Logs.info (fun m -> m "pulling from remote!");
+      Sync.pull store upstream `Set >>= fun r ->
+      Last_modified.retrieve_last_commit store >|= fun () ->
+      match r with
+      | Ok (`Head _ as s) -> Ok (Fmt.strf "pulled %a" Sync.pp_status s)
+      | Ok `Empty -> Error (`Msg "pulled empty repository")
+      | Error (`Msg e) -> Error (`Msg ("pull error " ^ e))
+      | Error (`Conflict msg) -> Error (`Msg ("pull conflict " ^ msg))
+  end
+
+  module Dispatch = struct
+    let dispatch store hook request _body =
+      let p = Uri.path (Cohttp.Request.uri request) in
+      let path = if String.equal p "/" then "index.html" else p in
+      Logs.info (fun f -> f "requested %s" path);
+      match Astring.String.cuts ~sep:"/" ~empty:false path with
+      | [ h ] when String.equal (Key_gen.hook ()) h ->
+        begin
+          hook () >>= function
+          | Ok data -> Http.respond ~status:`OK ~body:(`String data) ()
+          | Error (`Msg msg) ->
+            Http.respond ~status:`Internal_server_error ~body:(`String msg) ()
+        end
+      | path_list ->
+        if Last_modified.not_modified request then
+          Http.respond ~status:`Not_modified ~body:`Empty ()
+        else
+          Store.find store path_list >>= function
+          | Some data ->
+            let mime_type = Magic_mime.lookup path in
+            let headers = [
+              "content-type", mime_type ;
+              "etag", Last_modified.etag ;
+              "last-modified", Last_modified.last_modified ;
+            ] in
+            let headers = Cohttp.Header.of_list headers in
+            Http.respond ~headers ~status:`OK ~body:(`String data) ()
+          | None ->
+            let data = "Resource not found " ^ path in
+            Http.respond ~status:`Not_found ~body:(`String data) ()
+
+    let redirect port request _body =
+      let uri = Cohttp.Request.uri request in
+      let new_uri = Uri.with_scheme uri (Some "https") in
+      let new_uri = Uri.with_port new_uri (Some port) in
+      Logs.info (fun f -> f "[%s] -> [%s]"
+                    (Uri.to_string uri) (Uri.to_string new_uri));
+      let headers =
+        Cohttp.Header.init_with "location" (Uri.to_string new_uri)
+      in
+      Http.respond ~headers ~status:`Moved_permanently ~body:`Empty ()
+  end
 
   module LE = struct
     module Acme = Letsencrypt.Client.Make(Cohttp_mirage.Client)
@@ -186,7 +197,7 @@ module Main (S: Mirage_stack.V4) (RES: Resolver_lwt.S) (CON: Conduit_mirage.S) (
       let priv, csr = csr (Key_gen.cert_seed ()) (Key_gen.hostname ()) in
       let solver = Letsencrypt.Client.http_solver solver in
       Acme.sign_certificate ~ctx solver le sleep csr >|= fun certs ->
-      priv, certs
+      `Single (certs, priv)
   end
 
   let serve cb =
@@ -201,21 +212,20 @@ module Main (S: Mirage_stack.V4) (RES: Resolver_lwt.S) (CON: Conduit_mirage.S) (
     Lwt.map
       (function Ok () -> Lwt.return_unit | Error (`Msg msg) -> Lwt.fail_with msg)
       (let open Lwt_result.Infix in
-       connect_store resolver ssh_conduit >>= fun (store, upstream) ->
-       pull_store store upstream >>= fun data ->
-       Logs.info (fun m -> m "%s" data);
+       Remote.connect resolver ssh_conduit >>= fun (store, upstream) ->
+       Remote.pull store upstream >>= fun data ->
+       Logs.info (fun m -> m "store: %s" data);
        let http_port = Key_gen.port () in
        let tcp = `TCP http_port in
        let server =
-         let hook () = pull_store store upstream in
-         serve (dispatch store hook)
+         let hook () = Remote.pull store upstream in
+         serve (Dispatch.dispatch store hook)
        in
        if Key_gen.tls () then begin
-         Logs.info (fun f -> f "listening on 80/HTTP for let's encrypt provisioning");
-         (* this could be cancelled once certificates are retrieved *)
+         Logs.info (fun f -> f "listening on 80/HTTP (let's encrypt provisioning)");
+         (* this should be cancelled once certificates are retrieved *)
          Lwt.async (fun () -> http (`TCP 80) (serve LE.dispatch));
-         LE.provision_certificate resolver conduit >>= fun (priv, certs) ->
-         let certificates = `Single (certs, priv) in
+         LE.provision_certificate resolver conduit >>= fun certificates ->
          let tls_cfg = Tls.Config.server ~certificates () in
          let https_port = 443 in
          let tls = `TLS (tls_cfg, `TCP https_port) in
@@ -225,15 +235,12 @@ module Main (S: Mirage_stack.V4) (RES: Resolver_lwt.S) (CON: Conduit_mirage.S) (
          and http =
            Logs.info (fun f -> f "listening on %d/HTTP, redirecting to %d/HTTPS"
                          http_port https_port);
-           let redirect = serve (redirect https_port) in
+           let redirect = serve (Dispatch.redirect https_port) in
            http tcp redirect
          in
          Lwt_result.ok (Lwt.join [ https; http ])
        end else begin
-         let http =
-           Logs.info (fun f -> f "listening on %d/HTTP" http_port);
-           http tcp server
-         in
-         Lwt_result.ok http
+         Logs.info (fun f -> f "listening on %d/HTTP" http_port);
+         Lwt_result.ok (http tcp server)
        end)
 end
