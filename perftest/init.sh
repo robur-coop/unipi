2ec74052eac645e12c7868ead207de675a42fdde
rand00 <oth.rand@gmail.com>
2023-01-09 11:23:14 +0100

    perftest/init.sh: Using more unique name for networking setup

--- perftest/init.sh
+++ perftest/init.sh
@@ -4,20 +4,21 @@ set -e
 
 ROOT="$PWD"
 DATA_DIR=test-data
+TAP_NAME=unipi_tap
 GIT_REPO_NAME=unipi_web
 GIT_DAEMON_ROOT=git_daemon
 GIT_DAEMON_DIR="$GIT_DAEMON_ROOT"/"$GIT_REPO_NAME"
 GIT_CLIENT_DIR=git_client
-SERVICE=perf_service
+SERVICE=unipi_perf_service
 
 #// Networking
 
 sudo ip link add "$SERVICE" type bridge
 sudo ip addr add 10.0.0.1/24 dev "$SERVICE"
 sudo ip link set dev "$SERVICE" up
-sudo ip tuntap add tap100 mode tap
-sudo ip link set dev tap100 up
-sudo ip link set tap100 master "$SERVICE"
+sudo ip tuntap add "$TAP_NAME" mode tap
+sudo ip link set dev "$TAP_NAME" up
+sudo ip link set "$TAP_NAME" master "$SERVICE"
 
 #// Git repo with files to serve
 
