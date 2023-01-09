17db63685b42e38f607c3380edbfd3f71b932cd1
rand00 <oth.rand@gmail.com>
2023-01-09 11:42:44 +0100

    perftest/run-unikernel.sh: Setting + saving unipi mem-limit & renaming

--- perftest/run-unikernel.sh
+++ perftest/run-unikernel.sh
@@ -7,17 +7,23 @@ TAP_NAME=unipi_tap
 IP=10.0.0.2/24
 PORT=8888
 OUT=output
+#> Note:
+# * daemon is listening on all interfaces, so is on 10.0.0.1 too
+# * repo-name is defined in init.sh
+# * branch need to be passed to unipi, and is gits default
+REMOTE=git://10.0.0.1/unipi_web#master
+MEM=512 #MB
 
 if [ ! -e "$OUT" ]; then
     mkdir "$OUT"
 fi
 
-#goto save amount of memory assigned to unipi
+echo "$MEM" > "$OUT"/unipi_mem.txt
 
-solo5-hvt --net:service="$TAP_NAME" unipi.hvt \
+solo5-hvt --net:service="$TAP_NAME" --mem="$MEM" unipi.hvt \
           --ipv4 "$IP" --port "$PORT" \
-          --remote git://10.0.0.1/unipi_web#master \
-          2>&1 > "$OUT"/unipi.stdout
+          --remote "$REMOTE" \
+          2>&1 > "$OUT"/unipi_stdout.txt
 
 
 
