ca61a92d706fd621552bbd78e358c0dc6c518702
rand00 <oth.rand@gmail.com>
2023-01-17 11:23:20 +0100

    perftest/run-unikernel.sh: Fixed naming of tap-device + that unikernel didn't get enough mem ++

--- perftest/run-unikernel.sh
+++ perftest/run-unikernel.sh
@@ -2,19 +2,43 @@
 
 set -e
 
+prog_NAME=$(basename "${0}")
+
+warn()
+{
+    echo "${prog_NAME}: WARN: $*"
+}
+
+info()
+{
+    echo "${prog_NAME}: INFO: $*"
+}
+
+err()
+{
+    echo "${prog_NAME}: ERROR: $*" 1>&2
+}
+
+die()
+{
+    echo "${prog_NAME}: ERROR: $*" 1>&2
+    exit 1
+}
+
 # >>> CONFIGURE PER SYSTEM
 SOLO5=/home/rand/.opam/mirage-tlstunnel/bin/solo5-hvt
 # <<< --------------------
 
 #> Note: keep these in sync with test and init scripts
-TAP_NAME=unipi_tap
+TAP_NAME=unipitap
 IP=10.0.0.2/24
 PORT=8888
 OUT=output
 #> Note: port, repo-name, git branch-name need to be in sync with init.sh 
 GIT_DAEMON_PORT=6543
 REMOTE=git://10.0.0.1:"$GIT_DAEMON_PORT"/unipi_web.git#main
-MEM=128 #MB
+#> 128 too little
+MEM=512 #MB
 
 if [ ! -e "$OUT" ]; then
     mkdir "$OUT"
@@ -27,7 +51,8 @@ $SOLO5 --net:service="$TAP_NAME" --mem="$MEM" unipi.hvt \
           --remote "$REMOTE" \
           >"$OUT"/unipi_stdout.txt 2>&1 &
 
-echo $! > run-unikernel.sh.PID
+PID=$!
+echo "$PID" > "$prog_NAME".PID
 
 
 
